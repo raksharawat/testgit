76d6f33d9f0cf9ffda9494e795378ebbc018c4a6
raksharawat <46626114+raksharawat@users.noreply.github.com>
2019-01-19 18:03:23 +0530

    Add files via upload

---
+++ lec3/exercise/newcal.java
@@ -0,0 +1,24 @@
+//Write a program that takes two integers 
+//and displays the output of the following operators: 
+//addition, subtraction, multiplication, division and modulus.
+//values to be given within the program  
+class newcal
+{
+	public static void main(String args[])
+	{
+	int a,b;
+	a=7;
+    b=5;
+    System.out.print("Menu:");
+	System.out.print("\n 1. Addition: ");
+	System.out.println(a+b);
+	System.out.print("\n 2. Subtraction: ");
+	System.out.println(a-b);
+	System.out.print("\n 3. Multiplication: ");
+	System.out.println(a*b);
+	System.out.print("\n 4. Division: ");
+	System.out.println(a/b);
+	System.out.print("\n 5. Modulus: ");
+	System.out.println(a%b);
+	}
+}
