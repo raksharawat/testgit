41975e9e7c1c482296572a34245f960b7cb50205
raksharawat <46626114+raksharawat@users.noreply.github.com>
2019-01-19 17:56:01 +0530

    Add files via upload

---
+++ lec6/in class/distbw.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class dist
+{
+	 double dis;
+	 dist(int x1,int y1,int x2,int y2)
+	 {
+	  dis=(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+	 }
+}
+class distbw
+{
+	public static void main(String args[])
+	{
+       int x1,x2,y1,y2;
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("enter x1 point");
+       x1=sc.nextInt();
+       System.out.println("enter y1 point");
+       y1=sc.nextInt();
+       System.out.println("enter x2point");
+       x2=sc.nextInt();
+       System.out.println("enter y2 point");
+       y2=sc.nextInt();
+	   dist cd=new dist(x1,y1,x2,y2);	    
+  	   System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+cd.dis);
+	}
+}
