65f70e31dc703241d94b511a29e12a5883076b52
raksharawat <46626114+raksharawat@users.noreply.github.com>
2019-02-03 13:21:22 +0530

    Add files via upload

---
+++ lec9/exercise/Ex.java
@@ -0,0 +1,38 @@
+class BaseException extends Exception {}
+
+class Base 
+{
+	Base() throws BaseException 
+	{
+		throw new BaseException();
+	}
+}
+
+class Derived extends Base 
+{
+
+	Derived() throws BaseException 
+	{
+		super();
+		/*catch(BaseException e) {}		
+		try 
+		{
+		    super();
+		} catch(BaseException e) {}*/
+	}
+}
+
+public class Ex
+{
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			Derived d = new Derived();
+	} catch(BaseException e) 
+	{
+			System.out.println("BaseException caught in main()");
+	}
+			
+	}
+}
